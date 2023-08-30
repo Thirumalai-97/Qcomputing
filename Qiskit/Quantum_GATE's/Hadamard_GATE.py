647a33ea78fb59b5286f52d49ec293ba83115da6
Thirumalai <52080274+Thirumalai-97@users.noreply.github.com>
2023-08-30 11:38:54 +0530

    Update Hadamard_GATE.py
    3 Qubits has been modified to 2 qubit level

--- Qiskit/Quantum_GATE's/Hadamard_GATE.py
+++ Qiskit/Quantum_GATE's/Hadamard_GATE.py
@@ -1,1 +1,25 @@
+from qiskit import *
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import execute
+from qiskit.quantum_info import Statevector
+from qiskit.visualization import array_to_latex, plot_histogram
+from qiskit.quantum_info import Operator
 
+circuit=QuantumCircuit(2,2) #creating Q Circuit with required number of qubits
+circuit.h(0) # h() represents Hadamard GATE
+circuit.cx(0,1) # CX() represents Controlled NOT GATE
+
+circuit.measure([0,1],[0,1])
+circuit.draw('mpl') # 'mpl' is default used to draw circuit
+
+state=Statevector.from_int(0,2**2) # intializeing 2 qubits
+state=state.evolve(circuit)
+state.draw('latex') # latex is same as mpl
+
+array_to_latex(state)
+state.draw('qsphere')
+
+state.draw('hinton')
+
+op= Operator(circuit)
+op.data
