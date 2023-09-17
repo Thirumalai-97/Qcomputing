4f765963338b32c859513feb951cc56e56aa8a05
Thirumalai <52080274+Thirumalai-97@users.noreply.github.com>
2023-09-17 20:15:35 +0530

    Update Quantum_Gates.py

--- Qiskit/Quantum_GATE's/Quantum_Gates.py
+++ Qiskit/Quantum_GATE's/Quantum_Gates.py
@@ -1,9 +1,9 @@
 circuit=QuantumCircuit(2,2)
-circuit.x(0)
-circuit.x(1)
-circuit.y(1)
-circuit.z(0)
+circuit.x(0) # Pauli X GATE
+circuit.x(1) 
+circuit.y(1) # Pauli Y GATE
+circuit.z(0) # Pauli Z GATE
+circuit.h(0) # Hadamard GATE
 circuit.h(0)
-circuit.h(0)
-circuit.cx(0,1)
+circuit.cx(0,1) # Controlled NOT GATE
 circuit.draw('mpl')
